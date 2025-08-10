9e4ba2e4fea3ced66e318635654a99d30d7206b9
villarley <santivillarley1010@gmail.com>
2025-08-09 19:14:03 -0600

    feat: initialize Next.js demo app with dependencies

---
+++ demo/next.config.ts
@@ -0,0 +1,7 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ["passkey-kit", "passkey-kit-sdk", "sac-sdk"],
+};
+export default nextConfig;
